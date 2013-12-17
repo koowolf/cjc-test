53e883f09df5837ec5f21aa3bb4089c0deef88c6
koowolf <450928375@qq.com>
2013-12-17 15:58:26 +0800

    add string_unitest

---
+++ mygtest/mygtest/string_unitest.cpp
@@ -0,0 +1,40 @@
+#include "stdafx.h"
+#include "gtest.h"
+
+TEST(StringCmpTest, Demo)
+
+{
+
+    char* pszCoderZh = "CoderZh";
+
+    wchar_t* wszCoderZh = L"CoderZh";
+
+    std::string strCoderZh = "CoderZh";
+
+    std::wstring wstrCoderZh = L"CoderZh";
+
+
+
+    EXPECT_STREQ("CoderZh", pszCoderZh);
+
+    EXPECT_STREQ(L"CoderZh", wszCoderZh);
+
+
+
+    EXPECT_STRNE("CnBlogs", pszCoderZh);
+
+    EXPECT_STRNE(L"CnBlogs", wszCoderZh);
+
+
+
+    EXPECT_STRCASEEQ("coderzh", pszCoderZh);
+
+    //EXPECT_STRCASEEQ(L"coderzh", wszCoderZh);    ²»Ö§³Ö
+
+
+
+    EXPECT_STREQ("CoderZh", strCoderZh.c_str());
+
+    EXPECT_STREQ(L"CoderZh", wstrCoderZh.c_str());
+
+}
